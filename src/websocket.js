287e7533e234df704056de7718bdc6ab997e3f8d
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-04-06 23:48:21 +0800

    aws docker configuration

--- src/websocket.js
+++ src/websocket.js
@@ -1,3 +1,4 @@
-//const WS_URL = 'wss://redesign-backend-websocket.herokuapp.com/frontEnd';
-const WS_URL = "ws://localhost:8080/frontEnd";
+//const WS_URL = 'wss://redesign-backend-websocket.herokuapp.com/frontEnd'; //HEROKU
+//const WS_URL = "ws://localhost:8080/frontEnd"; //LOCALHOST
+const WS_URL ="ws://ec2-13-229-61-168.ap-southeast-1.compute.amazonaws.com:6565/frontEnd" //AWS
 export const ws = new WebSocket(WS_URL);
